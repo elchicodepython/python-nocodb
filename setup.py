7532b75c4787a80b55a7b336b5dff706c3340f7d
Samuel López Saura <samuellopezsaura@gmail.com>
2022-06-12 23:19:22 +0200

    new setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+setup(
+   name='nocodb',
+   version='0.0.1',
+   author='Samuel López Saura',
+   author_email='samuellopezsaura@gmail.com',
+   packages=find_packages(),
+   license='MIT',
+   url='https://github.com/ElChicoDePython/python-nocodb',
+   classifiers=[
+       "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+       "Operating System :: OS Independent",
+   ],
+   description='A package to use NocoDB API in a simple way',
+   long_description=open('README.md').read(),
+   long_description_content_type="text/markdown",
+   install_requires=[
+       "requests>=2.0",
+   ],
+)

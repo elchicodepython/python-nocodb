d970c03c58931a4f9c4ccba4a1d2bcb9f943ef26
Samuel López Saura <samuellopezsaura@gmail.com>
2023-03-03 20:54:47 +0100

    Add typing to exceptions file and format it

--- nocodb/exceptions.py
+++ nocodb/exceptions.py
@@ -1,5 +1,14 @@
+from typing import Optional
+
+
 class NocoDBAPIError(Exception):
-    def __init__(self, message, status_code, response_json=None, response_text=None):
+    def __init__(
+        self,
+        message: str,
+        status_code: int,
+        response_json: Optional[dict] = None,
+        response_text: Optional[str] = None,
+    ):
         super().__init__(message)
         self.status_code = status_code
         self.response_json = response_json

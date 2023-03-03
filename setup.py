b57f6c8b8956c7138373408af8d07aa3ecbefb3d
Samuel López Saura <samuellopezsaura@gmail.com>
2023-03-03 21:09:01 +0100

    Update package version to 1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='nocodb',
-   version='0.1.0',
+   version='1.0.1',
    author='Samuel López Saura',
    author_email='samuellopezsaura@gmail.com',
    packages=find_packages(),

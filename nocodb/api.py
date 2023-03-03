aee06320384bf2c5a0dfc2468f07445b967f414c
Jan Scheiper <janscheiper@protonmail.com>
2023-03-03 13:37:39 +0100

    use urljoin instead of string concatenation, added gitignore

--- nocodb/api.py
+++ nocodb/api.py
@@ -1,43 +1,38 @@
 from enum import Enum
+from urllib.parse import urljoin
 from .nocodb import NocoDBProject
 
 
 class NocoDBAPIUris(Enum):
-    V1_DB_DATA_PREFIX = "api/v1/db/data"
-    V1_DB_META_PREFIX = "api/v1/db/meta"
+    V1_DB_DATA_PREFIX = "api/v1/db/data/"
+    V1_DB_META_PREFIX = "api/v1/db/meta/"
 
 
 class NocoDBAPI:
     def __init__(self, base_uri: str):
-        self.__base_data_uri = (
-            f"{base_uri}/{NocoDBAPIUris.V1_DB_DATA_PREFIX.value}"
-        )
-        self.__base_meta_uri = (
-            f"{base_uri}/{NocoDBAPIUris.V1_DB_META_PREFIX.value}"
-        )
+        self.__base_data_uri = urljoin(base_uri + "/", NocoDBAPIUris.V1_DB_DATA_PREFIX.value)
+        self.__base_meta_uri = urljoin(base_uri + "/", NocoDBAPIUris.V1_DB_META_PREFIX.value)
 
     def get_table_uri(self, project: NocoDBProject, table: str) -> str:
-        return "/".join(
+        return urljoin(self.__base_data_uri, "/".join(
             (
-                self.__base_data_uri,
                 project.org_name,
                 project.project_name,
                 table,
             )
-        )
+        ))
 
     def get_row_detail_uri(
         self, project: NocoDBProject, table: str, row_id: int
     ):
-        return "/".join(
+        return urljoin(self.__base_data_uri, "/".join(
             (
-                self.__base_data_uri,
                 project.org_name,
                 project.project_name,
                 table,
                 str(row_id),
             )
-        )
+        ))
 
     def get_nested_relations_rows_list_uri(
         self,
@@ -47,9 +42,8 @@ class NocoDBAPI:
         row_id: int,
         column_name: str,
     ) -> str:
-        return "/".join(
+        return urljoin(self.__base_data_uri, "/".join(
             (
-                self.__base_data_uri,
                 project.org_name,
                 project.project_name,
                 table,
@@ -57,14 +51,9 @@ class NocoDBAPI:
                 relation_type,
                 column_name,
             )
-        )
+        ))
 
     def get_project_uri(
         self,
     ) -> str:
-        return "/".join(
-            (
-                self.__base_meta_uri,
-                "projects"
-            )
-        )
+        return urljoin(self.__base_meta_uri, "projects")
